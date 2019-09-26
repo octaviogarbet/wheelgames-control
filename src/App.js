8f08c2cf916819ef620b4f6ba303c6be80c837a2
Octavio <octaviogarbarino@gmail.com>
2019-09-26 00:57:05 -0300

    multiplayer feature on header done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.scss';
 import Footer from './components/footer';
 import Game from './components/game';
 import Header from './components/header';
 
-function App() {
-  return (
-    <div className="App">
-      <Header label="It's game time"/>
-      <Game team="Team 1"/>
-      <Footer/>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      totalTeams: 1,
+      teams: [{name: "Team 1", correct: 0, wrong: 0}]
+    };
+  }
+
+  handleTeamsChange = (totalTeams) => {
+    totalTeams = totalTeams > 4 ? 4 : Math.trunc(totalTeams);
+    totalTeams = totalTeams < 1 ? 1 : totalTeams;
+    const newTeams = [...this.state.teams];
+    let currentTotalTeams = this.state.totalTeams;
+    while (totalTeams > currentTotalTeams) {
+      currentTotalTeams++;
+      newTeams.push({name: 'Team '+currentTotalTeams, correct: 0, wrong: 0});
+    }
+
+    while (totalTeams < currentTotalTeams) {
+      newTeams.pop();
+      currentTotalTeams--;
+    }
+    this.setState({
+      teams: newTeams,
+      totalTeams: totalTeams
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header label="It's game time" teams={this.state.teams} totalTeams={this.state.totalTeams} onTeamsChange={this.handleTeamsChange}/>
+        <Game team="Team 1"/>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,8 +2,28 @@
 import React, { Component } from 'react'
 
 class Header extends Component {
+  handleChange = (event) => {
+    this.props.onTeamsChange(event.target.value);
+  }
+
   render() {
-    return <header><h1>{this.props.label}</h1></header>;
+    const teams = this.props.teams;
+    const scores = teams.map((team) =>
+      <div className="result-line"><span className="team-name">{team.name}</span> | <span className="score-correct">{team.correct}</span> | <span className="score-wrong">{team.wrong}</span></div>
+    );
+    return (
+    <header>
+      <div className="heading">
+        <h1>{this.props.label}</h1>
+      </div>
+      <div className="options">
+        <input type="number" value={this.props.totalTeams} onChange={this.handleChange} name="teams" id="teams" min="1" max="4" step="1"/>
+      </div>
+      <div className="score">
+        {scores}
+      </div>
+    </header>
+    );
   }
 }
 
