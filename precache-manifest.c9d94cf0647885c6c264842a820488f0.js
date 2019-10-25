fc9cd58642ff4c4fa1d1b4da6ffd24e51d64735f
Octavio <octaviogarbarino@gmail.com>
2019-10-24 23:33:45 -0300

    Updates

---
+++ precache-manifest.c9d94cf0647885c6c264842a820488f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e697807b3289cbb0b3104c1c8115f7e",
+    "url": "/wheelgames-control/index.html"
+  },
+  {
+    "revision": "3a8531b68f5750fbcb11",
+    "url": "/wheelgames-control/static/css/main.56ee27a3.chunk.css"
+  },
+  {
+    "revision": "f2f50ca92e9d1c451756",
+    "url": "/wheelgames-control/static/js/2.396cbddb.chunk.js"
+  },
+  {
+    "revision": "3a8531b68f5750fbcb11",
+    "url": "/wheelgames-control/static/js/main.e4ddee40.chunk.js"
+  },
+  {
+    "revision": "93835998092ee9459df7",
+    "url": "/wheelgames-control/static/js/runtime-main.53d097ce.js"
+  },
+  {
+    "revision": "14ed98e23dffa68d0c4fcd7b8cd1ea32",
+    "url": "/wheelgames-control/static/media/sprite.14ed98e2.svg"
+  }
+]);
