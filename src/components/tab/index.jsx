91e742f484e0c86ea3bd6cfa3335d99a4470c51f
Octavio <octaviogarbarino@gmail.com>
2019-10-26 17:59:49 -0300

    general layout styles fixed

--- src/components/tab/index.jsx
+++ src/components/tab/index.jsx
@@ -9,7 +9,7 @@ class Tab extends Component {
   render() {
     return (
     <div className={"tab " +this.props.className} onClick={this.handleClick}>
-      <span className="team-name">{this.props.team.name}</span> | <span className="score-correct">{this.props.team.correct}</span> | <span className="score-wrong">{this.props.team.wrong}</span>
+      <span className="team-name">{this.props.team.name}</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="score-correct">{this.props.team.correct}</span>&nbsp;&nbsp;<span className="score-wrong">{this.props.team.wrong}</span>
     </div>
     );
   }
